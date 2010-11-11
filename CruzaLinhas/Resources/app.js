ec34ade27b004c193a16bb4f0b342b0e919c5e37
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2010-11-11 20:55:15 -0200

    Refactored to routes module.

--- CruzaLinhas/Resources/app.js
+++ CruzaLinhas/Resources/app.js
@@ -1,3 +1,5 @@
+Ti.include('routes.js');
+
 var win = Titanium.UI.createWindow({  
     title:'CruzaLinhas',
     backgroundColor:'#fff'
@@ -11,7 +13,12 @@ var searchBar = Titanium.UI.createSearchBar({
 });
 win.add(searchBar);
 
-var saopaulo = { latitude: -23.564655852575104, longitude: -46.651318073272705, latitudeDelta: 0.05, longitudeDelta: 0.05 };
+var saopaulo = { 
+	latitude: -23.564655852575104, 
+	longitude: -46.651318073272705, 
+	latitudeDelta: 0.05, 
+	longitudeDelta: 0.05 
+};
 
 var annotation = Titanium.Map.createAnnotation({
 	latitude: saopaulo.latitude,
@@ -20,7 +27,7 @@ var annotation = Titanium.Map.createAnnotation({
 	animate: true
 });
 
-var mapview = Titanium.Map.createView({
+var mapView = Titanium.Map.createView({
 	mapType: Titanium.Map.STANDARD_TYPE,
 	region: saopaulo,
 	animate: true,
@@ -30,34 +37,7 @@ var mapview = Titanium.Map.createView({
 	top: 43
 });
 
-// read in our routes from a comma-separated file
-var f = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory,'.','route.csv');
-var csv = f.read();
-var points = [];
-var lines = csv.toString().split("\n");
-for (var c=0;c<lines.length;c++)
-{
-	var line = lines[c];
-	var latlong = line.split(",");
-	if (latlong.length > 1)
-	{
-		var lat = latlong[0];
-		var lon = latlong[1];
-		var entry = {latitude:lat,longitude:lon};
-		points[c]=entry;
-	}
-}
+routes.getRoutes(mapView);
 
-// route object
-var route = {
-	name:"boston",
-	points:points,
-	color:"red",
-	width:4
-};
-
-// add a route
-mapview.addRoute(route);
-
-win.add(mapview);
+win.add(mapView);
 win.open();
